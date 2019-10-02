8f1142b3605f44a3add20e9e39bab6978af4180f
Siarhei Prymakovich <prima.psa@gmail.com>
2019-10-03 00:02:31 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -39,7 +39,35 @@ const MORSE_TABLE = {
 
 function decode(expr) {  
 
+    let i=0, j=0, word = '', letter='', morse='', decode ='';
 
+    for(i=0; i<expr.length; i+=10){
+        word = expr.slice(i, i+10);
+        for(j=0; j<word.length; j+=2){
+            letter = word[j] + word[j+1];
+    
+            if (letter == '10'){
+                morse += '.'; 
+            }
+             else if (letter == '11'){
+                morse += '-';
+            }
+            else {
+                morse='';
+            }
+            letter = '';
+        }
+        if(morse.length > 0){
+            decode += MORSE_TABLE[morse];
+        }
+        else{
+            decode+= ' ';
+        }
+        morse = '';   
+    
+    }
+    return decode;
+    
 }
 
 module.exports = {
